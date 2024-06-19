2432e1fc8fb0ee8c59453314df68b1510f40b3c4
john-ryabikov <zhenya.ryabikov@mail.ru>
2024-06-19 15:43:36 +0300

    v.0.2.2

--- data/videos.js
+++ data/videos.js
@@ -1,17 +1,17 @@
 export const video_1 = [
     {
         id: 1,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 2,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 3,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     }
 ]
@@ -19,42 +19,42 @@ export const video_1 = [
 export const video_2 = [
     {
         id: 1,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 2,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 3,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 4,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 5,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 6,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 7,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 8,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     }
 ]
@@ -62,22 +62,22 @@ export const video_2 = [
 export const video_3 = [
     {
         id: 1,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 2,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 3,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 4,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     }
 ]
@@ -85,17 +85,17 @@ export const video_3 = [
 export const video_4 = [
     {
         id: 1,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 2,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 3,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     }
 ]
@@ -103,17 +103,17 @@ export const video_4 = [
 export const video_5 = [
     {
         id: 1,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 2,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     },
     {
         id: 3,
-        href: "/img/VideoBlock-3/video_3-1.mp4",
+        href: "/img/VideoBlock-3/video_3-1.MP4",
         prev: "bg-[url(/img/VideoBlock-3/video_3-1_prev.png)]"
     }
 ]
