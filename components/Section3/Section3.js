184db7cfc499026889e3429926bbe0659bb3ba94
John <64689339+john-ryabikov@users.noreply.github.com>
2024-06-24 08:53:37 +0300

    v.0.3.3

--- components/Section3/Section3.js
+++ components/Section3/Section3.js
@@ -32,7 +32,7 @@ export default function Section3() {
                 <VideoBlock3/>
         </WrapperCard>
         <h2 className='relative w-[335px] xl:w-[1200px] font-extralight text-white text-center text-3xl xl:text-[64px] my-12 xl:mb-[70px] xl:mt-[120px]'>Примеры отдельных артикулов</h2>
-        <ButtonMethod href="https://saleanalytics.tilda.ws" text="Примеры продвижения отдельных артикулов" img="/img/Icons/arrow_right_icon.svg" alt=">" color="bg-[#FF0066]"/>
+        <ButtonMethod href="https://saleanalytics.tilda.ws" text="Продвижение артикулов" img="/img/Icons/arrow_right_icon.svg" alt=">" color="bg-[#FF0066]"/>
     </SectionAnimated>
   )
 }
