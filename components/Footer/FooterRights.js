99d580a705c9114375abdff4d64d2fdc054799c5
John <64689339+john-ryabikov@users.noreply.github.com>
2025-10-30 08:54:11 +0300

    v1.3.2

--- components/Footer/FooterRights.js
+++ components/Footer/FooterRights.js
@@ -2,12 +2,12 @@ export default function FooterRights() {
   return (
     <div className='relative w-full flex flex-col xl:flex-row items-center justify-start xl:justify-between mb-10'>
         <div className='relative w-[130px] flex flex-row items-center justify-between mb-10 xl:mb-0'>
-        <img className='relative w-[50px]' src="/img/Section-1/ozon_logo.svg" alt="Ozon"/>
-        <img className='relative w-[60px]' src="/img/Section-1/wb_logo.png" alt="WB"/>
+          <img className='relative w-[50px]' src="/img/Section-1/ozon_logo.svg" alt="Ozon"/>
+          <img className='relative w-[60px]' src="/img/Section-1/wb_logo.png" alt="WB"/>
         </div>
         <p className='relative flex flex-col items-center xl:items-end justify-center text-xs text-end text-white font-extralight'>
-        <span className='mb-2'>Разработка:<a className='text-[#4E618A] font-bold ml-1 transition-all hover:text-[#1F5FE9]' href="https://just-site.ru" target="_blank" rel="nofollow">Just-site.ru</a></span>
-        <span>© 2022-2024 Все права защищены.</span>
+          <span className='mb-2'>Разработка:<a className='text-[#4E618A] font-bold ml-1 transition-all hover:text-[#1F5FE9]' href="https://just-site.ru" target="_blank" rel="nofollow">Just-site.ru</a></span>
+          <span>© 2022-{new Date().getFullYear()} Все права защищены.</span>
         </p>
   </div>
   )
