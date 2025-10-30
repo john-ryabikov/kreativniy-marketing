e7b5b7e8d78857e46ec8c03b91356a1c769bef26
John <64689339+john-ryabikov@users.noreply.github.com>
2025-10-30 09:01:13 +0300

    v1.3.5

--- components/Footer/FooterLinks.js
+++ components/Footer/FooterLinks.js
@@ -21,7 +21,7 @@ const links = [
 export default function FooterLinks() {
   return (
     <div className='relative w-full flex flex-col xl:flex-row items-center justify-start xl:justify-between'>
-        <p className='relative text-center xl:text-left font-extralight text-[#848484] text-base mb-8 xl:mb-0'>ИНН 505306466592<br/>ОГРН 321508100079173<br/>ИП ВЯЧЕСЛАВ БЫЧКОВ ВАДИМОВИЧ</p>
+        <p className='relative text-center xl:text-left font-extralight text-[#848484] text-base mb-8 xl:mb-0'>ИНН 503124024765<br/>ОГРНИП 325508100452210<br/>ИП ТИМОШИНА ИРИНА АЛЕКСАНДРОВНА</p>
         <p className='relative flex flex-col items-center xl:items-end font-extralight text-[#848484] text-xs'>
         {links.map((link, i) => (
             <Link key={i} className='relative inline-block mb-1 last:mb-0 xl:text-right hover:text-[#C0C0C0] transition-all' href={link.href} target="_blank">{link.cont}</Link>
